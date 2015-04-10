b8bae712ab5cd4924b86f0c271b4464b46cbc5d7
RubenDjOn <ruben@indahouser.com>
2015-04-10 06:41:21 +0200

    Fixed name on Tests

--- test_python_diario_software.py
+++ test_python_diario_software.py
@@ -5,22 +5,22 @@ import python_diario
 
 class TestPythonSoftware(unittest.TestCase):
 
-    def test_should_return_python_when_number_is_divisble_by_3(self):
+    def test_should_return_python_when_number_is_3(self):
         self.assertEqual('Python', python_diario.get_string(3))
 
-    def test_should_return_python_when_number_is_divisble_by_6(self):
+    def test_should_return_python_when_number_is_6(self):
         self.assertEqual('Python', python_diario.get_string(6))
 
-    def test_should_return_python_when_number_is_divisble_by_9(self):
+    def test_should_return_python_when_number_is_9(self):
         self.assertEqual('Python', python_diario.get_string(9))
 
-    def test_should_return_diario_when_number_is_divisble_by_5(self):
+    def test_should_return_diario_when_number_is_5(self):
         self.assertEqual('Diario', python_diario.get_string(5))
 
-    def test_should_return_diario_when_number_is_divisble_by_10(self):
+    def test_should_return_diario_when_number_is_10(self):
         self.assertEqual('Diario', python_diario.get_string(10))
 
-    def test_should_return_diario_when_number_is_divisble_by_20(self):
+    def test_should_return_diario_when_number_is_20(self):
         self.assertEqual('Diario', python_diario.get_string(20))
 
     def test_should_return_7_when_number_is_7(self):
